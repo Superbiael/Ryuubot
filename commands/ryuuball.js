29c1bc8e31f0d5e1f9c919dd82f2b63b8e38c4e3
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-08-19 23:21:36 -0700

    Update ryuuball.js

--- commands/ryuuball.js
+++ commands/ryuuball.js
@@ -8,7 +8,6 @@ module.exports.run = async (bot, message, args) => {
    let question = args.slice(0).join(" ");
 
    let ballembed = new Discord.RichEmbed()
-   .setAuthor(message.author.tag)
    .setColor("#213e59")
    .addField("Question:", question)
    .addField("Answer:", replies[result]);

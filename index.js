e0b9c7e29713e44e27c0c32e29389e18491e5d76
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-09-12 23:34:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ bot.on('message', message => {
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args);
 
-     let botschannel = message.guild.channels.find(`name`, "idolbot7");
-     if(!botschannel) return;
+   let botschannel = message.guild.channels.find(`name`, "bot-channel");
+   if(!botschannel) return;
   
      msg = message.content.toLowerCase();
      mention = message.mentions.users.first();

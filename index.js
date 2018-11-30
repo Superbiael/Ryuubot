47a18916c44b0d7bd0463851b50a3c054aa740ae
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-11-30 01:03:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.on('message', message => {
      mention = message.mentions.users.first();
 
       if (msg.startsWith (prefix + "scout")) {
-        number = 68;
+        number = 69;
         imageNumber = Math.floor (Math.random() * (number)) + 1;
         botschannel.send ({files: ["./scout/" + imageNumber + ".png"]})
       }
